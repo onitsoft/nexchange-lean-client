858afdb34a9b2e71de3a4e45350b08251ab97d71
Oleg Belousov <oleg@onit.ws>
2017-08-12 23:20:10 +0300

    eclare undeclared modules

--- src/app/index.components.js
+++ src/app/index.components.js
@@ -1,11 +1,18 @@
 'use strict';
 
 import headerModule from './components/header/header.module';
-import addressModule from './components/address/address.module';
 import footerModule from './components/footer/footer.module';
+
+import coinModule from './components/coin/coin.module';
+import coinInputModule from './components/coin_input/coininput.module';
+import exchangeWidgetModule from './components/exchange_widget/exchangewidget.module';
+import addressModule from './components/address/address.module';
 
 export default angular.module('index.components', [
   headerModule.name,
+  coinModule.name,
+  coinInputModule.name,
+  exchangeWidgetModule.name,
   addressModule.name,
   footerModule.name,
 ]);
